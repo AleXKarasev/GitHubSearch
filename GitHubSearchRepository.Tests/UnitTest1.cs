5d6ac7f0c3ac71f8d7bdabfdb286361a8a700f9b
AleXK <alexk.karasev@gmail.com>
2016-04-10 08:24:15 +0300

    Add empty project

---
+++ GitHubSearchRepository.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitHubSearchRepository.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
